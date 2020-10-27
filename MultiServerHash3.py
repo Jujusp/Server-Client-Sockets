d2903b5ac709092e17ef70b9f3f23fdf9c65b0cd
Nicolás Tobo <na.tobo@uniandes.edu.co>
2020-10-27 17:15:45 -0500

    Añadiendo opcion video 3

--- MultiServerHash3.py
+++ MultiServerHash3.py
@@ -132,6 +132,7 @@ threads = []
 print("Hola!, bienvenido a la aplicacion del grupo 11, por favor selecciona el archivo de video a mandar: "+"\n")
 print("1. Video 1 de  100 MB"+"\n")
 print("2. Video 2 de  250 MB"+"\n")
+print("3. Video 3 de  2 MB"+"\n")
 opcion = int(input("Ingresa una opcion: "))
 fileGlobal = 'ventilador_100.mp4' if (opcion == 1) else 'hielo_250.mp4' if (opcion == 2) else 'secuencia.mp4'
 print("Listo, menciona el numero de clientes a los que quieres antender en simultaneo para enviar el archivo: "+"\n")
